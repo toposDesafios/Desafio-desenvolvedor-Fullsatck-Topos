179ef5c06e6e56c96c40e8640ea3bf01da236e46
Vulcis-Jonas <Jonas@gmail.com>
2021-11-13 18:37:10 -0300

    Insert - Back e Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
 
   const [nameEmployee, setNameEmployee] = useState('');
   const [birthDateEmployee, setBirthDateEmployee] = useState('');
+  const [numRgEmployee, setNumRgEmployee] = useState('');
+  const [numCpfEmployee, setNumCpfEmployee] = useState('');
+  const [nameMotherEmployee, setNameMotherEmployee] = useState('');
+
+  const setSubmit = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      nome: nameEmployee, 
+      data_nascimento: birthDateEmployee, 
+      num_rg: numRgEmployee, 
+      num_cpf: numCpfEmployee, 
+      nome_mae: nameMotherEmployee, 
+    }).then(() => {
+      alert('teste');
+    });
+  };
 
   return (
     <div className="App">
       <h1>Desafio</h1>
-      <input type="text" onChange={(e) => {setNameEmployee(e.target.value)}}></input>
-      <input type="date" onChange={(e) => {setBirthDateEmployee(e.target.value)}}></input>
+      <div className="list-inputs">
+        <input type="text" name="nome" onChange={(e) => {setNameEmployee(e.target.value)}}></input>
+        <input type="date" name="data_nascimento" onChange={(e) => {setBirthDateEmployee(e.target.value)}}></input>
+        <input type="text" name="num_rg" onChange={(e) => {setNumRgEmployee(e.target.value)}}></input>
+        <input type="text" name="num_cpf" onChange={(e) => {setNumCpfEmployee(e.target.value)}}></input>
+        <input type="text" name="nome_mae" onChange={(e) => {setNameMotherEmployee(e.target.value)}}></input>
+        <input type="submit" onClick={setSubmit}></input>
+      </div>
       {nameEmployee}
       {birthDateEmployee}
+      {numRgEmployee}
+      {numCpfEmployee}
+      {nameMotherEmployee}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 
@@ -11,18 +13,28 @@ const db = new sqlite3.Database(db_name, err => {
   }
   console.log("Successful connection to the database 'database.db'");
 });
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.post("/api/insert", (req, res) => {
+  console.log(req);
+  const sql_insert = `INSERT INTO Funcionario (data_nascimento, nome, nome_mae, num_cpf, num_rg) VALUES
+  (?, ?, ?, ?, ?);`;
+  db.run(sql_insert, [req.body.data_nascimento, req.body.nome, req.body.nome_mae, req.body.num_cpf, req.body.num_rg] ,(err, result) => {
+    console.log('sucess');
+  })
+ });
 
 app.listen(3001, () => { 
   console.log("Server started (http://localhost:3001/) !");
 });
 
-app.get("/", (req, res) => { 
-  // Database seeding
-  const sql_insert = `INSERT INTO Funcionario (data_nascimento, nome, nome_mae, num_cpf, num_rg) VALUES
-  ('1994-12-07', 'Jonas', 'Helena', '05549951584', '1108169715');`;
-  db.run(sql_insert, (err, result) => {
-    res.send ("Hello world...");
-  })
-});
+// app.get("/", (req, res) => { 
+//   // Database seeding
+//   const sql_insert = `INSERT INTO Funcionario (data_nascimento, nome, nome_mae, num_cpf, num_rg) VALUES
+//   ('1994-12-07', 'Jonas', 'Helena', '05549951584', '1108169715');`;
+//   db.run(sql_insert, (err, result) => {
+//     res.send ("Hello world...");
+//   })
+// });
