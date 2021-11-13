e4bc7bc3ac1d4432d65502c39b89d9ac1c14e7eb
Vulcis-Jonas <Jonas@gmail.com>
2021-11-13 13:41:07 -0300

    use state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [nameEmployee, setNameEmployee] = useState('');
+  const [birthDateEmployee, setBirthDateEmployee] = useState('');
+
   return (
     <div className="App">
       <h1>Desafio</h1>
-      Hello
+      <input type="text" onChange={(e) => {setNameEmployee(e.target.value)}}></input>
+      <input type="date" onChange={(e) => {setBirthDateEmployee(e.target.value)}}></input>
+      {nameEmployee}
+      {birthDateEmployee}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const app = express();
 
 const sqlite3 = require("sqlite3").verbose();
 
-const db_name = path.join(__dirname, "/database/", "db.db");
+const db_name = path.join(__dirname, "/database/", "database.db");
 const db = new sqlite3.Database(db_name, err => {
   if (err) {
     return console.error(err.message);
   }
-  console.log("Successful connection to the database 'db.db'");
+  console.log("Successful connection to the database 'database.db'");
 });
 
 
