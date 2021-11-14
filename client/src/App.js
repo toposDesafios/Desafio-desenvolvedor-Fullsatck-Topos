14b16c3a10f03c0e6603b24970e73efe82b5abc2
Vulcis-Jonas <Jonas@gmail.com>
2021-11-14 01:25:29 -0300

    create dependents

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,28 @@ import Axios from 'axios';
 function App() {
 
   const [dataDB, setDataDB] = useState([]);
+  const [codFuncionario, setCodFuncionario] = useState('');
   const [nameEmployee, setNameEmployee] = useState('');
   const [birthDateEmployee, setBirthDateEmployee] = useState('');
   const [numRgEmployee, setNumRgEmployee] = useState('');
   const [numCpfEmployee, setNumCpfEmployee] = useState('');
   const [nameMotherEmployee, setNameMotherEmployee] = useState('');
 
-  const setSubmit = () => {
-    Axios.post('http://localhost:3001/api/insert', {
+  const setSubmitEmployee = () => {
+    Axios.post('http://localhost:3001/api/insertEmployee', {
+      nome: nameEmployee, 
+      data_nascimento: birthDateEmployee, 
+      num_rg: numRgEmployee, 
+      num_cpf: numCpfEmployee, 
+      nome_mae: nameMotherEmployee, 
+    }).then(() => {
+      alert('teste');
+    });
+  };
+
+  const setSubmitDependent = () => {
+    Axios.post('http://localhost:3001/api/insertDependent', {
+      cod_funcionario: codFuncionario, 
       nome: nameEmployee, 
       data_nascimento: birthDateEmployee, 
       num_rg: numRgEmployee, 
@@ -33,15 +47,33 @@ function App() {
   return (
     <div className="App">
       <h1>Desafio</h1>
+      <h2>Funcionario</h2>
       <div className="list-inputs">
         <input type="text" name="nome" onChange={(e) => {setNameEmployee(e.target.value)}}></input>
         <input type="date" name="data_nascimento" onChange={(e) => {setBirthDateEmployee(e.target.value)}}></input>
         <input type="text" name="num_rg" onChange={(e) => {setNumRgEmployee(e.target.value)}}></input>
         <input type="text" name="num_cpf" onChange={(e) => {setNumCpfEmployee(e.target.value)}}></input>
         <input type="text" name="nome_mae" onChange={(e) => {setNameMotherEmployee(e.target.value)}}></input>
-        <input type="submit" onClick={setSubmit}></input>
+        <input type="submit" onClick={setSubmitEmployee}></input>
+      </div>
+
+      <h2>Dependente</h2>
+      <div className="list-inputs">
+        <select name="cod_funcionario" onChange={(e) => {setCodFuncionario(e.target.value)}}>
+          {dataDB.map((val)=> {
+            return <option value={val.cod_funcionario}>{val.nome}</option>
+          })}
+        </select>
+        <input type="text" name="nome" onChange={(e) => {setNameEmployee(e.target.value)}}></input>
+        <input type="date" name="data_nascimento" onChange={(e) => {setBirthDateEmployee(e.target.value)}}></input>
+        <input type="text" name="num_rg" onChange={(e) => {setNumRgEmployee(e.target.value)}}></input>
+        <input type="text" name="num_cpf" onChange={(e) => {setNumCpfEmployee(e.target.value)}}></input>
+        <input type="text" name="nome_mae" onChange={(e) => {setNameMotherEmployee(e.target.value)}}></input>
+        <input type="submit" onClick={setSubmitDependent}></input>
       </div>
       
+      
+      <h2>Lista</h2>
       <div className="list-inputs">
         {dataDB.map((val)=> {
           return <li>Nome: {val.nome}</li>
--- server/index.js
+++ server/index.js
@@ -19,26 +19,39 @@ const db = new sqlite3.Database(db_name, err => {
   console.log("Success database.db");
 });
 
-app.post("/api/insert", (req, res) => {
+app.post("/api/insertEmployee", (request, response) => {
   const sql_insert = `INSERT INTO Funcionario (data_nascimento, nome, nome_mae, num_cpf, num_rg) VALUES
   (?, ?, ?, ?, ?);`;
-  db.run(sql_insert, [req.body.data_nascimento, req.body.nome, req.body.nome_mae, req.body.num_cpf, req.body.num_rg] ,(err, result) => {
-    
-    console.log('sucess insert');
-  })
- });
-
-app.get("/api/getEmployee", (req, res) => {
-  const sql_select = `SELECT * FROM Funcionario;`;
-  db.all(sql_select, (err, result) => {
+  db.run(sql_insert, [request.body.data_nascimento, request.body.nome, request.body.nome_mae, request.body.num_cpf, request.body.num_rg] ,(err, result) => {
     if (err) {
       return console.error(err.message);
     }
-    res.send(result);
-    //console.log(result);
+    return response.status(200).json('Employee inserted');
+  })
+ });
+
+app.post("/api/insertDependent", (request, response) => {
+  const sql_insert = `INSERT INTO Dependente (cod_funcionario, data_nascimento, nome, nome_mae, num_cpf, num_rg) VALUES
+  (?, ?, ?, ?, ?, ?);`;
+  db.run(sql_insert, [request.body.cod_funcionario, request.body.data_nascimento, request.body.nome, request.body.nome_mae, request.body.num_cpf, request.body.num_rg] ,(err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    return response.status(200).json('Dependent inserted');
+  })
+ });
+
+app.get("/api/getEmployee", (request, response) => {
+  const sql_select = `SELECT * FROM Funcionario;`;
+  db.all(sql_select, (err, result) => {
+    if (err) {
+      return response.status(500).json({ err });
+    }
+    return response.status(200).json( result );
   });
 });
 
 app.listen(3001, () => { 
   console.log("Server started (http://localhost:3001/)!");
 });
+
