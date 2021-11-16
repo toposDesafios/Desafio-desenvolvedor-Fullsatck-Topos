0a1ddf6dd55cad0c79315adc6b99410be0ada0a7
Vulcis-Jonas <Jonas@gmail.com>
2021-11-16 08:54:46 -0300

    Toggle - Front

--- frontend/src/components/ToggleRegister/index.js
+++ frontend/src/components/ToggleRegister/index.js
@@ -2,13 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
+import { makeStyles } from '@material-ui/styles';
 import './index.css';
+import { red } from '@mui/material/colors';
+
+const useStyles = makeStyles({
+  root: {
+    width: '400px',
+    height: '50px',
+    position: 'absolute', 
+    margin: 'auto auto auto -350px',      
+    transform: 'rotate(-90deg)',
+    background: red,
+  },
+  button: {
+    width: '200px',
+    height: '50px',
+  }
+});
 
 function ToggleRegister() {
+  const classes = useStyles();
   return (
-    <ButtonGroup variant="contained" orientation="vertical">
-      <Button>Funcionario</Button>
-      <Button>Dependente</Button>
+    <ButtonGroup variant="contained" className={classes.root}>
+      <Button className={classes.button}>Funcionario</Button>
+      <Button className={classes.button}>Dependente</Button>
     </ButtonGroup>
   );
 }
--- frontend/src/containers/Register/index.js
+++ frontend/src/containers/Register/index.js
@@ -1,86 +1,91 @@
-// import React, { useState, useEffect } from 'react';
-// import FormEmployee from '../../components/FormEmployee';
-// import FormDependent from '../../components/FormDependent';
-// import ToggleRegister from '../../components/ToggleRegister';
-// import './index.css';
-
-// function Register() {
-
-
-//   return (
-//     <div className="register-content">
-//       <ToggleRegister></ToggleRegister>
-//       <FormEmployee></FormEmployee>''
-//       <FormDependent></FormDependent>
-//     </div>
-//   );
-// }
-
-// export default Register;
-
-
 import React, { useState, useEffect } from 'react';
 import FormEmployee from '../../components/FormEmployee';
 import FormDependent from '../../components/FormDependent';
 import ToggleRegister from '../../components/ToggleRegister';
 import './index.css';
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/styles";
 
-class ProfileTabs extends React.PureComponent {
-  state = { activeIndex: 0 };
+function Register() {
 
-  handleChange = (_, activeIndex) => this.setState({ activeIndex });
-  render() {
-    const { activeIndex } = this.state;
-    return (
-      <div
-        style={{
-          display: "flex"
-        }}
-      >
-        <VerticalTabs value={activeIndex} onChange={this.handleChange}>
-          <MyTab label="item one" />
-          <MyTab label="item two" />
-        </VerticalTabs>
 
-        {activeIndex === 0 && <TabContainer><FormEmployee></FormEmployee></TabContainer>}
-        {activeIndex === 1 && <TabContainer><FormDependent></FormDependent></TabContainer>}
-      </div>
-    );
-  }
-}
-
-const VerticalTabs = withStyles(theme => ({
-  flexContainer: {
-    flexDirection: "column"
-  },
-  indicator: {
-    display: "none"
-  }
-}))(Tabs);
-
-const MyTab = withStyles(theme => ({
-  root: {
-    backgroundColor: "#ccc"
-  },
-  wrapper: {
-    backgroundColor: "#ddd"
-  },
-  selected: {
-    color: "tomato",
-    borderBottom: "2px solid tomato"
-  }
-}))(Tab);
-
-function TabContainer(props) {
   return (
-    <Typography component="div" style={{ padding: 24 }}>
-      {props.children}
-    </Typography>
+    <div className="register-content">
+      <ToggleRegister></ToggleRegister>
+      {/* <FormEmployee></FormEmployee>'' */}
+      <FormDependent></FormDependent>
+    </div>
   );
 }
 
-export default ProfileTabs;
+export default Register;
+
+
+// import React, { useState, useEffect } from 'react';
+// import FormEmployee from '../../components/FormEmployee';
+// import FormDependent from '../../components/FormDependent';
+// import ToggleRegister from '../../components/ToggleRegister';
+// import './index.css';
+// import Tabs from "@mui/material/Tabs";
+// import Tab from "@mui/material/Tab";
+// import Typography from "@material-ui/core/Typography";
+// import { withStyles } from "@material-ui/styles";
+
+// class ProfileTabs extends React.PureComponent {
+//   state = { activeIndex: 0 };
+
+//   handleChange = (_, activeIndex) => this.setState({ activeIndex });
+//   render() {
+//     const { activeIndex } = this.state;
+//     return (
+//       <div
+//         style={{
+//           display: "flex"
+//         }}
+//       >
+//         <Tabs value={activeIndex} onChange={this.handleChange} aria-label="basic tabs example">
+//           <Tab label="Item One" />
+//           <Tab label="Item Two"/>
+//           <Tab label="Item Three"/>
+//         </Tabs>
+//         <VerticalTabs value={activeIndex} onChange={this.handleChange}>
+//           <MyTab label="Funcionario" />
+//           <MyTab label="Dependente" />
+//         </VerticalTabs>
+
+//         {activeIndex === 0 && <TabContainer><FormEmployee></FormEmployee></TabContainer>}
+//         {activeIndex === 1 && <TabContainer><FormDependent></FormDependent></TabContainer>}
+//       </div>
+//     );
+//   }
+// }
+
+// const VerticalTabs = withStyles(theme => ({
+//   flexContainer: {
+//     flexDirection: "column"
+//   },
+//   indicator: {
+//     display: "none"
+//   }
+// }))(Tabs);
+
+// const MyTab = withStyles(theme => ({
+//   root: {
+//     backgroundColor: "#ccc"
+//   },
+//   wrapper: {
+//     backgroundColor: "#ddd"
+//   },
+//   selected: {
+//     color: "tomato",
+//     borderBottom: "2px solid tomato"
+//   }
+// }))(Tab);
+
+// function TabContainer(props) {
+//   return (
+//     <Typography component="div" style={{ padding: 24 }}>
+//       {props.children}
+//     </Typography>
+//   );
+// }
+
+// export default ProfileTabs;
